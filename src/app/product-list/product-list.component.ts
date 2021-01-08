b8d14fe346dcac6a2c0790c7ec58d16974d6ab4a
npeirano <nacho.peirano@gmail.com>
2021-01-07 21:30:20 -0300

    2 files modified

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 
 import { products } from '../products';
 
+import { FormBuilder } from '@angular/forms';
+
+
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
@@ -9,10 +12,28 @@ import { products } from '../products';
 })
 export class ProductListComponent {
   products = products;
+  checkoutForm;
+
+
+  constructor(
+    private formBuilder: FormBuilder,
+  ) {
+    this.checkoutForm = this.formBuilder.group({
+      name: ''    });
+  }
 
   share() {
     window.alert('The product has been shared!');
   }
+
+  onSubmit(customerData) {
+    // Process checkout data here
+    this.checkoutForm.reset();
+
+    console.warn('Your order has been submitted', customerData);
+  }
+
+  
 }
 
 
