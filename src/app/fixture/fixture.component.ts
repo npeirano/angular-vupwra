fc5bde91f2da1ab29f68fb8e203d2ce9e638b506
npeirano <nacho.peirano@gmail.com>
2021-01-11 17:15:50 -0300

    1 file modified

--- src/app/fixture/fixture.component.ts
+++ src/app/fixture/fixture.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AdministrarEquiposService } from '../administrar-equipos.service';
 import { Equipo } from '../equipo';
 import { ActivatedRoute } from '@angular/router';
+import { Partido } from '../partido';
 
 
 @Component({
@@ -10,13 +11,19 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./fixture.component.css']
 })
 export class FixtureComponent implements OnInit {
-
+  fecha: Array<Partido>;
   constructor(private administrarEquiposService: AdministrarEquiposService, private route: ActivatedRoute,
 ) {
 
    }
 
-  ngOnInit() {
-  }
+ ngOnInit() {
+  // First get the product id from the current route.
+  const routeParams = this.route.snapshot.paramMap;
+  const productIdFromRoute = Number(routeParams.get('fechaId'));
+
+  // Find the product that correspond with the id provided in route.
+  this.product = products.find(product => product.id === productIdFromRoute);
+}
 
 }
