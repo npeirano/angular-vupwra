3d0c104de9b65ad3988ac3fab3b8334804830873
npeirano <nacho.peirano@gmail.com>
2021-01-11 16:29:06 -0300

    1 file modified

--- src/app/administrar-equipos.service.ts
+++ src/app/administrar-equipos.service.ts
@@ -23,6 +23,27 @@ export class AdministrarEquiposService {
   constructor() { }
 
 
-  crearFixu
+  createMatches(){
+    let partido : Partido;
+    for(let i = 0; i < this.equipos.length; i++){
+        let fecha = new Array<Partido> ();
+        let equipoActual = this.equipos[i]; // Hold team in a variable
+        // Iterate through all teams
+        for(let j = 0; j < this.equipos.length; j++){
+            // If the teams are the same continue iterating
+            if(equipoActual.nombre == this.equipos[j].nombre){
+                continue;
+            }else {
+            // If teams are from same town, hold the teams in a temporary derby variable
+                partido.local = equipoActual;
+                partido.visitante = this.equipos[j];
+                // Go through all matches checking if both teams have played before
+                fecha.push(partido);
+            }
+        }
+        this.fixture.push(fecha);
+        // Add derby match after the current team has already played with all other teams
+    }
+}
 
 }
