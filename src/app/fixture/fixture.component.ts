cd57a3bc9dfc2b1ffe81f412ec25adc3f961e1f4
npeirano <nacho.peirano@gmail.com>
2021-01-11 17:09:20 -0300

    1 file modified

--- src/app/fixture/fixture.component.ts
+++ src/app/fixture/fixture.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AdministrarEquiposService } from '../administrar-equipos.service';
 import { Equipo } from '../equipo';
+import { ActivatedRoute } from '@angular/router';
 
 
 @Component({
@@ -10,8 +11,9 @@ import { Equipo } from '../equipo';
 })
 export class FixtureComponent implements OnInit {
 
-  constructor(private administrarEquiposService: AdministrarEquiposService) {
-    
+  constructor(private administrarEquiposService: AdministrarEquiposService, private route: ActivatedRoute,
+) {
+
    }
 
   ngOnInit() {
